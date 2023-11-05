46d708ca42175f026965efed1cf479c0c4492c52
Yassine AHMAIMI <yassine.ahmaimi@edu.esiee.fr>
2023-11-05 20:09:18 +0100

    Appui BTs

--- app/src/main/java/com/example/eventadvisor/MainActivity.java
+++ app/src/main/java/com/example/eventadvisor/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.eventadvisor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton monImageButtonSport;
+    ImageButton monImageButtonMusique;
+    //FirebaseFirestore firestore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String,Object> users = new HashMap<>();
+        users.put("firstName","Test");
+
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"Sucess",Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"Failure",Toast.LENGTH_LONG).show();
+            }
+        });
+*/
+        monImageButtonSport = (ImageButton) findViewById(R.id.bp_sports);
+        monImageButtonMusique = (ImageButton) findViewById(R.id.bp_musique);
+
+        monImageButtonSport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadSportActivity = new Intent(MainActivity.this, SportActivity.class);
+                startActivity(intentLoadSportActivity);
+            }
+        });
+
+        monImageButtonMusique.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadMusiqueActivity = new Intent(MainActivity.this, MusiqueActivity.class);
+                startActivity(intentLoadMusiqueActivity);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/eventadvisor/MusiqueActivity.java
@@ -0,0 +1,14 @@
+package com.example.eventadvisor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusiqueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musique);
+    }
+}
